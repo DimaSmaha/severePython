9e2bf7855d5bcc9e4654ad49702649e4274f3343
DimaSmaha <dmytrosmaha@gmail.com>
2025-04-22 16:40:22 +0300

    add one more test

--- tests/add_challenge.test.py
+++ tests/add_challenge.test.py
@@ -1,6 +1,11 @@
 import pytest
-from ..challenges.add_challenge import add
+from challenges.add_challenge import add
 
 
 def should_add_positive_numbers():
     assert add(5, 6) == 11
+
+
+def should_show_error_for_not_int():
+    with pytest.raises(TypeError):
+        assert add('a', 2)
