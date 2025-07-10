c38850e278b55e9b33defbae02436a7f3bd37a3c
DimaSmaha <dmytrosmaha@gmail.com>
2025-07-10 20:11:35 +0300

    test s2

--- tests/test_s2_find_lucky_number.py
+++ tests/test_s2_find_lucky_number.py
@@ -22,3 +22,37 @@ def test_should_return_noTickets():
 def test_should_return_typeError():
     with pytest.raises(TypeError):
         assert get_lucky_ticket_numbers('')
+
+
+def test_valid_lucky_ticket():
+    assert get_lucky_ticket_numbers(["001001"]) == ["001001"]
+
+
+def test_valid_unlucky_ticket():
+    assert get_lucky_ticket_numbers(["123456"]) == noTicketsMessage
+
+
+def test_invalid_ticket_too_short():
+    assert get_lucky_ticket_numbers(["12345"]) == noTicketsMessage
+
+
+def test_invalid_ticket_non_numeric():
+    assert get_lucky_ticket_numbers(["12a456"]) == noTicketsMessage
+
+
+def test_ticket_out_of_range_zero():
+    assert get_lucky_ticket_numbers(["000000"]) == noTicketsMessage
+
+
+def test_ticket_out_of_range_high():
+    assert get_lucky_ticket_numbers(["100001"]) == noTicketsMessage
+
+
+def test_mixed_valid_and_invalid():
+    tickets = ["001001", "123456", "abc123", "000000", "100000"]
+    assert get_lucky_ticket_numbers(tickets) == ["001001"]
+
+
+def test_less_than_min_symbols():
+    tickets = ["12345"]
+    assert get_lucky_ticket_numbers(tickets) == noTicketsMessage
