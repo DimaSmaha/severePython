2df1dbb73ffff5b40520d32f8a6f17b13767b027
DimaSmaha <dmytrosmaha@gmail.com>
2025-10-16 21:39:42 +0300

    last session for 2025 add pw

---
+++ tests/playwright/test_example_async.py
@@ -0,0 +1,8 @@
+# import pytest
+# from playwright.async_api import Page
+
+
+# @pytest.mark.asyncio(loop_scope="session")
+# async def test_foo(page: Page):
+#     await page.goto("https://github.com")
+#     # ...
